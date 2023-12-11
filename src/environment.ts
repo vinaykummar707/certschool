005067eddd7907d8bc21509d32066b933722a51e
vinaykummar707 <vinaykummar707@gmail.com>
2023-12-12 00:09:24 +0530

    new node js

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ export  const environment = {
     homePageTitle : "Adarsh Forum",
     homePageRoute: "./students",
     homePageIcon:"https://images.pexels.com/photos/18103225/pexels-photo-18103225/free-photo-of-horses-in-prairie.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    serverUrl: "https://adarsh-cert-school-server.azurewebsites.net",
+    serverUrl: "https://adarsh-forum-node-js-app.azurewebsites.net/",
     // serverUrl: "http://localhost:8080",
     registerPageTitle:"Adarsh Forum - Student Registration",
     educationTypes:{
